641c367d9adb5a12fbde781c75b471052101f1e1
yodermonique <monique.yoder@gmail.com>
2019-11-11 18:56:17 -0500

    changes to data frames

--- ANOVA starter_Sand_demo.R
+++ ANOVA starter_Sand_demo.R
@@ -7,16 +7,16 @@ rm(list=ls()) #clear environment
 
 #import the data
 library(haven)
-d <- read_sav("Obarow.Story2.sav")
+DP <- read_sav("Obarow.Story2.sav")
 
 #Create Music as an IV
-d$music<-ifelse(d$trtmnt2==1, "No", ifelse(d$trtmnt2==2, "No", "Yes"))
+DP$music<-ifelse(d$trtmnt2==1, "No", ifelse(d$trtmnt2==2, "No", "Yes"))
 
 #Do the same for picture
 ###Your code here######
 
 #computer gain scores
-d$gain<-d$postest2-d$pretest2
+DP$gain<-d$postest2-d$pretest2
 
 #get your dataset ready for analysis (subject id and other things)
 
