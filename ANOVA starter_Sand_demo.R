3b6638546ae5da883579d1b8c6485d87096d612a
Monique Yoder <yodermonique@Moniques-MacBook-Air.local>
2019-11-11 18:48:01 -0500

    Changed the data frame template

---
+++ ANOVA starter_Sand_demo.R
@@ -0,0 +1,23 @@
+##Classwork starter
+getwd()
+
+setwd("~/Dropbox/MSU SLS/LLT 873/Week10_Demo")
+
+rm(list=ls()) #clear environment
+
+#import the data
+library(haven)
+d <- read_sav("Obarow.Story2.sav")
+
+#Create Music as an IV
+d$music<-ifelse(d$trtmnt2==1, "No", ifelse(d$trtmnt2==2, "No", "Yes"))
+
+#Do the same for picture
+###Your code here######
+
+#computer gain scores
+d$gain<-d$postest2-d$pretest2
+
+#get your dataset ready for analysis (subject id and other things)
+
+###GOOD LUCK##########
